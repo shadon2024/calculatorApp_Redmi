69b08e193fe9ec78baf0b37ba70c24a62460ba37
shadon2024 <in-con23@yandex.ru>
2024-05-19 18:01:56 +0500

    add fail constans

---
+++ calculatorApp_Redmi/Constans.swift
@@ -0,0 +1,15 @@
+//
+//  Constans.swift
+//  calculatorApp
+//
+//  Created by Admin on 12/05/24.
+//
+
+
+import Foundation
+
+enum ConstansString {
+    static let CalculatorTitle = "0"
+    
+
+}
